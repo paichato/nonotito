53473256eab8647bfcab85110e9381a24ed52252
Marlon Strong <tsenane14@gmail.com>
2021-12-16 02:52:30 +0200

    added lib +

--- src/lib/fonts.tsx
+++ src/lib/fonts.tsx
@@ -4,9 +4,17 @@ import {
   Poppins_500Medium,
   Poppins_600SemiBold,
 } from "@expo-google-fonts/poppins";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 export default {
   regular: "Poppins_400Regular",
   medium: "Poppins_500Medium",
   semibold: "Poppins_600SemiBold,",
+  p: hp("2%"),
+  txt: hp("2.5%"),
+  h1: hp("3.5%"),
+  h2: hp("3%"),
 };
