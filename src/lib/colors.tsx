93c124f0efd98bbc06f9ed1cfce6c14493324ae7
Marlon Strong <tsenane14@gmail.com>
2021-12-16 02:48:22 +0200

    added lib

---
+++ src/lib/colors.tsx
@@ -0,0 +1,10 @@
+export default {
+  bg: "#1D1D28",
+  bg_white: "#DEDEE6",
+  primary: "#18172B",
+  secondary: "#C49753",
+  grad1: "#FFFFFF",
+  grad2: "#EEF0F3",
+  // text: "#72737B",
+  unselected: "#848BAD",
+};
---
+++ src/lib/fonts.tsx
@@ -0,0 +1,12 @@
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+} from "@expo-google-fonts/poppins";
+
+export default {
+  regular: "Poppins_400Regular",
+  medium: "Poppins_500Medium",
+  semibold: "Poppins_600SemiBold,",
+};
