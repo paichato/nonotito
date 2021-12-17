92dd1298292cff8f10d8955099b8a16a71d92d86
Marlon Strong <tsenane14@gmail.com>
2021-12-17 04:10:53 +0200

    added tweaks + +

--- src/screens/Opening/index.tsx
+++ src/screens/Opening/index.tsx
@@ -5,6 +5,8 @@ import {
   TouchableOpacity,
   KeyboardAvoidingView,
   Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
 import {
   AddressHeader,
@@ -34,10 +36,11 @@ export default function Opening() {
   const [focus, setFocus] = useState(false);
 
   const handleFocus = () => {
-    setFocus(true);
+    if (!focus) setFocus(true);
   };
 
   const handleUnFocus = () => {
+    // Keyboard.dismiss();
     setFocus(false);
   };
 
@@ -48,140 +51,173 @@ export default function Opening() {
   };
 
   return (
-    <Container source={require("../../assets/bg.png")}>
-      <KeyboardAvoidingView
-        // enabled={false}
-        keyboardVerticalOffset={Platform.OS === "ios" ? 10 : 0}
-        style={{
-          width: "100%",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-      >
-        <Animatable.View
+    <TouchableWithoutFeedback
+    //  onPress={handleUnFocus}
+    >
+      <Container source={require("../../assets/bg.png")}>
+        <KeyboardAvoidingView
+          // enabled={false}
+          keyboardVerticalOffset={Platform.OS === "ios" ? 10 : -10}
           style={{
             width: "100%",
             alignItems: "center",
             justifyContent: "center",
           }}
-          animation={!clicked ? "pulse" : "slideInUp"}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
         >
-          <>{!focus && <Logo />}</>
-        </Animatable.View>
+          <Animatable.View
+            style={{
+              width: "100%",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            animation={!clicked ? "pulse" : "slideInUp"}
+          >
+            <>{!focus && <Logo />}</>
+          </Animatable.View>
 
-        {!clicked && (
-          <>
-            <Animatable.View
-              style={{ width: "100%" }}
-              animation={!clicked ? "fadeInUp" : "fadeOutRightBig"}
-            >
-              <BigTitle>Aonde deseja{"\n"} pedir hoje?</BigTitle>
-              <Txt color="white">Digite o CEP de entrega</Txt>
-            </Animatable.View>
-            <Animatable.View
-              style={{ width: "100%" }}
-              animation={!clicked ? "zoomIn" : "zoomOut"}
-            >
-              <InputContainer activeOpacity={1} onPress={handleCEP}>
-                <>
-                  <Input
-                    selectionColor={colors.secondary}
-                    inlineImageLeft="../../assets/buscar.png"
-                  />
-                  <SearchIcon />
-                </>
-              </InputContainer>
-            </Animatable.View>
-            <Animatable.View
-              style={{ width: "100%" }}
-              animation={!clicked ? "fadeIn" : "fadeOut"}
-            >
-              <TouchableOpacity>
-                <Txt>Não sabe o seu CEP?</Txt>
-              </TouchableOpacity>
-            </Animatable.View>
-          </>
-        )}
-
-        {clicked && (
-          <RegisterContainer>
-            {!focus && (
-              <Animatable.View animation="fadeInDown">
-                <AddressHeader>
-                  <IconWrapper>
-                    <Pin />
-                  </IconWrapper>
-                  <></>
-                  <AddressWrapper>
-                    <Txt color="white" position="left">
-                      Rua Três de Maio, 50 - Jardim Olavo Bilac São Bernardo do
-                      Campo - SP 09725-810 - Brazil
-                    </Txt>
-                  </AddressWrapper>
-                </AddressHeader>
-              </Animatable.View>
-            )}
-            <Animatable.View useNativeDriver animation="fadeInLeft" delay={100}>
-              <Txt position="left" color="white">
-                Endereço
-              </Txt>
-            </Animatable.View>
-            <Animatable.View useNativeDriver animation="zoomIn" delay={100}>
-              <TxtInput onFocus={handleFocus} onBlur={handleUnFocus} />
-            </Animatable.View>
-            <Animatable.View useNativeDriver animation="fadeInLeft" delay={300}>
-              <Txt position="left" color="white">
-                Bairro
-              </Txt>
-            </Animatable.View>
-            <Animatable.View useNativeDriver animation="zoomIn" delay={300}>
-              <TxtInput onFocus={handleFocus} onBlur={handleUnFocus} />
-            </Animatable.View>
-            <AddressHeader>
-              <View style={{ width: "48%", marginTop: 30 }}>
-                <Animatable.View
-                  useNativeDriver
-                  animation="fadeInLeft"
-                  delay={500}
-                >
-                  <Txt position="left" color="white">
-                    Número
-                  </Txt>
-                </Animatable.View>
-                <Animatable.View useNativeDriver animation="zoomIn" delay={500}>
-                  <TxtInput onFocus={handleFocus} onBlur={handleUnFocus} />
-                </Animatable.View>
-              </View>
-              <View style={{ width: "48%", marginTop: 30 }}>
-                <Animatable.View
-                  useNativeDriver
-                  animation="fadeInRight"
-                  delay={700}
-                >
-                  <Txt position="left" color="white">
-                    Complemento
-                  </Txt>
-                </Animatable.View>
-                <Animatable.View useNativeDriver animation="zoomIn" delay={700}>
-                  <TxtInput onFocus={handleFocus} onBlur={handleUnFocus} />
-                </Animatable.View>
-              </View>
-            </AddressHeader>
+          {!clicked && (
             <>
               <Animatable.View
-                useNativeDriver
-                animation="slideInUp"
-                delay={1000}
+                style={{ width: "100%" }}
+                animation={!clicked ? "fadeInUp" : "fadeOutRightBig"}
               >
-                <RegisterButton>
-                  <Txt color="white">Fazer Pedido</Txt>
-                </RegisterButton>
+                <BigTitle>Aonde deseja{"\n"} pedir hoje?</BigTitle>
+                <Txt color="white">Digite o CEP de entrega</Txt>
+              </Animatable.View>
+              <Animatable.View
+                style={{ width: "100%" }}
+                animation={!clicked ? "zoomIn" : "zoomOut"}
+              >
+                <InputContainer activeOpacity={1} onPress={handleCEP}>
+                  <>
+                    <Input
+                      selectionColor={colors.secondary}
+                      inlineImageLeft="../../assets/buscar.png"
+                    />
+                    <SearchIcon />
+                  </>
+                </InputContainer>
+              </Animatable.View>
+              <Animatable.View
+                style={{ width: "100%" }}
+                animation={!clicked ? "fadeIn" : "fadeOut"}
+              >
+                <TouchableOpacity>
+                  <Txt>Não sabe o seu CEP?</Txt>
+                </TouchableOpacity>
               </Animatable.View>
             </>
-          </RegisterContainer>
-        )}
-      </KeyboardAvoidingView>
-    </Container>
+          )}
+
+          {clicked && (
+            <RegisterContainer>
+              {!focus && (
+                <Animatable.View animation="fadeInDown">
+                  <AddressHeader>
+                    <IconWrapper>
+                      <Pin />
+                    </IconWrapper>
+                    <></>
+                    <AddressWrapper>
+                      <Txt color="white" position="left">
+                        Rua Três de Maio, 50 - Jardim Olavo Bilac São Bernardo
+                        do Campo - SP 09725-810 - Brazil
+                      </Txt>
+                    </AddressWrapper>
+                  </AddressHeader>
+                </Animatable.View>
+              )}
+              <Animatable.View
+                useNativeDriver
+                animation="fadeInLeft"
+                delay={100}
+              >
+                <Txt position="left" color="white">
+                  Endereço
+                </Txt>
+              </Animatable.View>
+              <Animatable.View useNativeDriver animation="zoomIn" delay={100}>
+                <TxtInput
+                  // onPressOut={handleUnFocus}
+                  onFocus={handleFocus}
+                  onSubmitEditing={handleUnFocus}
+                />
+              </Animatable.View>
+              <Animatable.View
+                useNativeDriver
+                animation="fadeInLeft"
+                delay={300}
+              >
+                <Txt position="left" color="white">
+                  Bairro
+                </Txt>
+              </Animatable.View>
+              <Animatable.View useNativeDriver animation="zoomIn" delay={300}>
+                <TxtInput
+                  onFocus={handleFocus}
+                  onSubmitEditing={handleUnFocus}
+                />
+              </Animatable.View>
+              <AddressHeader>
+                <View style={{ width: "48%", marginTop: 30 }}>
+                  <Animatable.View
+                    useNativeDriver
+                    animation="fadeInLeft"
+                    delay={500}
+                  >
+                    <Txt position="left" color="white">
+                      Número
+                    </Txt>
+                  </Animatable.View>
+                  <Animatable.View
+                    useNativeDriver
+                    animation="zoomIn"
+                    delay={500}
+                  >
+                    <TxtInput
+                      onFocus={handleFocus}
+                      onSubmitEditing={handleUnFocus}
+                    />
+                  </Animatable.View>
+                </View>
+                <View style={{ width: "48%", marginTop: 30 }}>
+                  <Animatable.View
+                    useNativeDriver
+                    animation="fadeInRight"
+                    delay={700}
+                  >
+                    <Txt position="left" color="white">
+                      Complemento
+                    </Txt>
+                  </Animatable.View>
+                  <Animatable.View
+                    useNativeDriver
+                    animation="zoomIn"
+                    delay={700}
+                  >
+                    <TxtInput
+                      onFocus={handleFocus}
+                      onSubmitEditing={handleUnFocus}
+                    />
+                  </Animatable.View>
+                </View>
+              </AddressHeader>
+              <>
+                <Animatable.View
+                  useNativeDriver
+                  animation="slideInUp"
+                  delay={1000}
+                >
+                  <RegisterButton>
+                    <Txt color="white">Fazer Pedido</Txt>
+                  </RegisterButton>
+                </Animatable.View>
+              </>
+            </RegisterContainer>
+          )}
+        </KeyboardAvoidingView>
+      </Container>
+    </TouchableWithoutFeedback>
   );
 }
