5a8ce2f6ba054cef9e360939afb41efb8c753297
Marlon Strong <tsenane14@gmail.com>
2022-01-09 22:17:10 +0200

    added cart structure

--- src/screens/Cart/index.tsx
+++ src/screens/Cart/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { View, Text } from "react-native";
 import ButtonWrapper from "../../components/ButtonWrapper";
 import MainHeader from "../../components/MainHeader";
-import { Banner, BannerLeft, Container, MainContainer } from "./styles";
+import { Banner, BannerLeft, Container, CupomContainer, MainContainer, PizzaDetails, PizzaImage, PizzaWrapper, SubTotalWrapper } from "./styles";
 import BackIcon from "../../assets/back.svg";
 import { Txt } from "../Opening/styles";
+
+import { SharedElement } from "react-navigation-shared-element";
 
 export default function Cart({navigation}) {
 
@@ -12,8 +14,62 @@ export default function Cart({navigation}) {
     navigation.goBack();
   };
 
+  const cart = [
+    {
+      id: 0,
+      name: "Marguerita",
+      price: "47,50",
+      img: require("../../assets/pizzas/pizza.png"),
+    },
+    {
+      id: 1,
+      name: "Guaraná",
+      price: "10,00",
+      img: require("../../assets/refris/refri4.png"),
+    },
+  ];
+
+
+  const handleItemSelect = (id) => {
+    // const selectedData = popularPizzas.filter((item) => item.id === id);
+    // navigation.navigate("PizzaDetails", { item: selectedData[0] });
+    console.log(id);
+    
+  };
+
+  const CartItem=({item})=>{
+    return (
+<PizzaWrapper
+                  onPress={() => handleItemSelect(item.id)}
+                  
+                >
+                  
+                    <PizzaImage
+                      style={{ resizeMode: "cover", width: "25%", height: "100%" }}
+                      resizeMode="contain"
+                      source={item.img}
+                      // source={require('../../assets/pizzas/pizza6.jpg')}
+                    />
+                
+
+                  <PizzaDetails style={{width:'40%'}}>
+                    <Txt position="left" margin color="blue" bold>
+                      {/* Marguerita */}
+                      {item.name}
+                    </Txt>
+                  
+                  </PizzaDetails  >
+                  
+                    <Txt margin color="blue" bold>
+                      R$ {item.price}
+                    </Txt>
+                   
+                </PizzaWrapper>
+    )
+  }
+
   return (
-    <Container>
+    <Container source={require('../../assets/bg-white.png')} >
       <MainHeader>
       <ButtonWrapper action={handleGoBack} Icon={<BackIcon />} />
           <Txt
@@ -40,6 +96,54 @@ export default function Cart({navigation}) {
           </Txt>
 
         </Banner>
+        {cart.map((item)=>{
+          return(
+            <CartItem item={item} />
+          )
+
+        })
+          
+        }
+
+        <CupomContainer>
+          <Txt margin color='blue' bold >
+            Cupom
+          </Txt>
+          <Txt margin color='white'size='h1'>+</Txt>
+        </CupomContainer>
+        <SubTotalWrapper>
+        <Txt margin color='white'  >
+            Subtotal
+          </Txt>
+          <Txt margin color='white'  >
+          R$ 57,50
+          </Txt>
+        </SubTotalWrapper>
+        <SubTotalWrapper>
+        <Txt margin color='white'  >
+            Subtotal
+          </Txt>
+          <Txt margin color='white'  >
+          R$ 57,50
+          </Txt>
+        </SubTotalWrapper>
+        <SubTotalWrapper>
+        <Txt margin color='white'  >
+            Subtotal
+          </Txt>
+          <Txt margin color='white'  >
+          R$ 57,50
+          </Txt>
+        </SubTotalWrapper>
+        <SubTotalWrapper>
+        <Txt margin color='blue' bold >
+            Subtotal
+          </Txt>
+          <Txt margin color='blue' bold size='h2' >
+          R$ 57,50
+          </Txt>
+        </SubTotalWrapper>
+        
       </MainContainer>
     </Container>
   );
--- src/screens/Cart/styles.ts
+++ src/screens/Cart/styles.ts
@@ -3,10 +3,10 @@ import {
     widthPercentageToDP as wp,
     heightPercentageToDP as hp,
   } from "react-native-responsive-screen";
-import { TouchableOpacity } from "react-native";
+import { ImageBackground, TouchableOpacity } from "react-native";
 import colors from "../../lib/colors";
 
-export const Container=styled.View`
+export const Container=styled(ImageBackground)`
 flex:1;
 
 `
@@ -33,7 +33,53 @@ justify-content:space-around;
 `
 
 export const BannerLeft=styled.View`
-width: 70%;
+width: 60%;
 align-items:flex-start;
 
+`
+
+export const PizzaWrapper=styled.TouchableOpacity`
+width: 100%;
+height: ${hp('10%')}px;
+/* background-color:${colors.grad1}; */
+border-radius:16px;
+flex-direction: row;
+align-items: center;
+justify-content:space-between;
+margin-bottom: ${hp('1.5%')}px;
+padding:0px 5px;
+margin-top: ${hp('1.5%')}px;
+`
+
+export const PizzaImage=styled.Image`
+width: 25%;
+border-radius:16px;
+
+`
+
+export const PizzaDetails=styled.View`
+/* width: 30%; */
+`
+
+export const CupomContainer=styled(TouchableOpacity)`
+width: 100%;
+height: ${hp('8%')}px;
+background-color:${colors.unselected_light};
+flex-direction: row;
+align-items: center;
+justify-content:space-between;
+padding:0px ${wp('5%')}px;
+border-radius: 10px;
+margin-top:${hp('1.5%')}px;
+margin-bottom:${hp('1.5%')}px;
+
+`
+
+export const SubTotalWrapper=styled.View`
+width: 100%;
+flex-direction: row;
+align-items: center;
+justify-content:space-between;
+padding:0px ${wp('5%')}px;
+margin-bottom:${hp('1.5%')}px;
 `
